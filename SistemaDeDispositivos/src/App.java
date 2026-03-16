bfcb954ab142d0780fea727a7f5790f3c6f5e832
Rigoberto Sanchez Jimenez <20243ds185@utez.edu.mx>
2026-03-15 20:16:53 -0600

    ultimo ejercicio

--- SistemaDeDispositivos/src/App.java
+++ SistemaDeDispositivos/src/App.java
@@ -1,8 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Bateria b1 = new Bateria(5000,"Lionn");
+        Bateria b2 = new Bateria(4800,"Markup");
+
+        Dispositivo d1 = new Telefono("Xiaomi","Xiamo redmi note 10 pro",b1,4);
+        Dispositivo d2 = new Laptop("Asus","Asus tuf gamin",b2,2);
+
+        Dispositivo[] dispositivos = {d1,d2};
+
+        Conectable c1 = new Laptop("Asus","Asus tuf gamin",b2,2);
+
+        for(Dispositivo d: dispositivos){
+            System.out.println(d.getMarca());
+            System.out.println(d.consumoEnergia());
+            if (d instanceof Conectable){
+                ((Conectable) d).conectarWifi();
+            }
+        }
 
 
+        Laptop l1 = (Laptop) d2;
+        ((Laptop) d2).conectarWifi();
+        l1.conectarWifi();
+
+//        Encendible e = new Moto("Suzuki",45,m2,true);
+//
+//        if(e instanceof Moto){
+//            Moto m = (Moto) e;
+//            m.acelerar(10);
+//        }
 
     }
 }
